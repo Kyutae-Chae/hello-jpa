803a64c6a415917275a0a266f6e8f89995d7a01d
Kyutae-Chae <ktchae83@gmail.com>
2022-06-29 13:20:54 +0900

    NA

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,14 +24,22 @@ public class JpaMain {
 ////        em.remove(member);
 //        member.setName("HelloJPA");
 
-        List<Member> select_m_from_member = em.createQuery("select m from Member as m", Member.class).getResultList();
-        for (Member member : select_m_from_member) {
-            System.out.println("member.getName() = " + member.getName());
-            
-        }
-        tx.commit();
+        try {
+            Member member = new Member();
+            member.setUsername("AAA");
+            Member member2 = new Member();
+            member.setUsername("BBB");
 
-        em.clear();
+            em.persist(member);
+            em.persist(member2);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+//        em.clear(); //영속성 컨텍스트 전체 삭제
         emf.close();
     }
 }
